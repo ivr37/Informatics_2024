2151200b5dbc9b5623b4c08a820820fa1d8bbec9
Ivan <ivanrumin611@gmail.com>
2024-11-03 13:38:26 +0300

    fixed lab4 and added lab4

--- golang/lab6/lab6.go
+++ golang/lab6/lab6.go
@@ -1,12 +1,6 @@
 package lab6
 
-<<<<<<< HEAD
-import (
-	"fmt"
-)
-=======
 import "fmt"
->>>>>>> 0c6b35e (fixed lab4 and added lab6)
 
 type Phone struct {
 	Color  string
@@ -23,18 +17,6 @@ func NewPhone(color, os, number string) *Phone {
 }
 
 func (p *Phone) SetNumber(number string) { p.Number = number }
-<<<<<<< HEAD
-func (p Phone) GetOS() string            { return p.OS }
-func (p Phone) GetColor() string         { return p.Color }
-func (p Phone) GetNumber() string        { return p.Number }
-
-func RunLab6() {
-	honor := NewPhone("Белый", "Андроид", "89203568472")
-	honor.SetNumber(("89203657254"))
-	fmt.Println("У нас есть телефон с операционной системой", honor.GetOS())
-	fmt.Println("Цвет телефона:", honor.GetColor())
-	fmt.Println("Номер телефона:", honor.GetNumber())
-=======
 func (p Phone) GetNumber() string        { return p.Number }
 func (p Phone) GetOS() string            { return p.OS }
 func (p Phone) GetColor() string         { return p.Color }
@@ -45,5 +27,4 @@ func RunLab6() {
 	fmt.Println("У нас есть мобильный телефон марки honor с операционной системой", honor.GetOS())
 	fmt.Println("Цвет телефона:", honor.GetColor())
 	fmt.Println("Номер телефона пользователя:", honor.GetNumber())
->>>>>>> 0c6b35e (fixed lab4 and added lab6)
 }
